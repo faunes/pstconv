c08b6c9d95267f89e298a3b981648a4056f3ee14
TIMSONET John-Arthur <timsonetja@debian-vbox>
2014-08-26 00:23:08 +0200

    Ai rajouté une ligne

---
+++ contacts_pstconvert.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Extrait les contacts d'une archive pst et les convertit en vcf.
+
+
+mkdir cont_$(echo $1) && cd cont_$(echo $1)
+
+echo "Je bosse pour toi mon gars, un peu de patience."
+
+readpst -e -D -cv -t c -o . ../$1 > log_$(echo $1)
+find . -type d -empty -exec rmdir {} \; 2>> log_$(echo $1)
+
+echo "Terminé !"
+
+exit
