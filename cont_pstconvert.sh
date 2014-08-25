35acf5f6c79cbcc88ed33a0a2f71dcac1026550b
TIMSONET John-Arthur <faunes@gmail.com>
2014-08-26 01:09:15 +0200

    Ajout de commentaires

--- cont_pstconvert.sh
+++ cont_pstconvert.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 # Extrait les contacts d'un fichier pst et les convertit en vcf.
 
 
@@ -9,6 +8,10 @@ mkdir cont_$(echo $1) && cd cont_$(echo $1)
 echo "Je bosse pour toi mon gars, un peu de patience."
 
 readpst -e -D -cv -t c -o . ../$1 > log_$(echo $1)
+
+
+# Suppression des dossiers vides résiduels
+
 find . -type d -empty -exec rmdir {} \; 2>> log_$(echo $1)
 
 echo "Terminé !"
